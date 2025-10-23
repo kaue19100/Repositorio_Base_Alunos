2d3a55391ad46302990c80353a4fa1e3b9cdcb4d
kaue19100 <kaueoliveira1910@gmail.com>
2025-10-23 08:21:48 -0300

    Add files via upload

---
+++ Modulo 2/Manipulação de Arquivos e Automação/Projeto_gerenciador_de_tarefas/gerenciador_de_tarefas_V2.py
@@ -0,0 +1,80 @@
+import os
+import json
+
+with open("tarefas.json", "r", encoding="utf-8")as arquivo:
+    tarefas = json.load(arquivo)
+
+
+def mostrar_tarefas():
+    barra = f'| {60* '-'}|'
+    print(barra)
+    for i in range(len(tarefas['tarefas'])):
+        print(f'| {i+1} - Tarefas: {tarefas['tarefas'] [i] } | Datas: {tarefas ['datas'] [i]}')
+    input('Aperte ENTER para continuar...')
+
+def adicionar_tarefas():
+    barra = f'| {60* '-'}|'
+    print(barra)
+    tarefa = input('Adicione a tarefa:    ')
+    data = input('Coloque a data em que a tarefa tem que ser cumprida:    ' )
+    tarefas['tarefas'].append(tarefa)
+    tarefas['datas'].append(data)
+    print('Tarefa adicionada com sucesso !!!')
+    with open("tarefas.json", "w", encoding="utf-8" ) as arquivo:
+        json.dump(tarefas, arquivo, indent=4, ensure_ascii=False)
+
+    print('Adicionar tarefas')
+    input('Aperte ENTER para continuar...')
+def remover_tarefas():
+    barra = f'| {60* '-'}|'
+    print(barra)
+    for i in range(len(tarefas['tarefas'])):
+        print(f'| {i+1} - Tarefas: {tarefas['tarefas'] [i] } | Datas: {tarefas ['datas'] [i]}') 
+    id_tarefa = int(input('| Digite o número da tarefa que deseja remover:   '))
+    if id_tarefa > 0: 
+        tarefa = tarefas['| tarefas'].pop(id_tarefa - 1)
+        tarefas['| datas'].pop(id_tarefa - 1)
+    else: 
+        print('| ID invalido ')
+    print(f'| tarefa "{tarefa}" removida com sucesso !!!')
+    with open("tarefas.json", "w", encoding="utf-8" ) as arquivo:
+        json.dump(tarefas, arquivo, indent=4, ensure_ascii=False)
+    print(barra)
+    input('| Aperte ENTER para continuar...')
+    print('| Remover tarefas')
+    input('| Aperte ENTER para continuar...')
+
+def menu():
+    while True: 
+        try:
+            os.system('cls')
+            barra = f'| {60* '-'}|'
+            print(barra)
+            print('|GERENCIADOR DE TAREFAS')
+            print(barra)
+            print('| 1 - Mostrar tarefas')
+            print('| 2 - Adicionar tarefas')
+            print('| 3 - Remover tarefas ')
+            print('| 4 - Sair')
+            print(barra)
+            opc = int(input('|Escolha uma opção:   '))
+            if opc == 1:
+                os.system('cls')
+                mostrar_tarefas()
+            elif opc == 2:
+                os.system('cls')
+                adicionar_tarefas()
+            elif opc == 3:
+                os.system('cls')
+                remover_tarefas()
+            elif opc == 4:
+                print('|Saindo do programa..... ')
+                break
+            else:
+                os.system('cls')
+                print('|Opção invalida')
+                input('|Aperte ENTER para continuar...')
+        except: 
+            print('ERRO digite uma opção valida !!!   ')
+            input('|Aperte ENTER para continuar...')
+menu()
